e8387e38954ba8fde624ac32263e48ce8af28129
VardgesHambaryan <vardges.hambaryan@gmail.com>
2022-12-01 15:25:40 +0400

    V1

--- Django/core/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-urlpatterns = [ 
-    path('', ContactListView.as_view(), name= 'home' ),
-    path('login/' , LoginListView.as_view() , name= 'login')
-
-]
